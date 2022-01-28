3bb79f00a32b0994dc0c6e0a77ba7e527019d28c
viachaskul <fixrapdoK@gmail.com>
2022-01-28 10:10:41 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =
+
+    function check(str, bracketsConfig) {
+        let sync = {
+            open: {},
+            close: {}
+        };
+        bracketsConfig.forEach(e => {
+            sync.open[e[0]] = e[1];
+            sync.close[e[1]] = e[0];
+        });
+
+        let arr = str.split('');
+        arr.push('end');
+        arr.unshift('end');
+
+        return checker(arr, sync) || false;
+    }
+
+function checker(arr, bracketsConfig) {
+
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+
+        let brLeft = bracketsConfig.close[element];
+
+        if (brLeft) {
+            if (arr[index - 1] == brLeft) {
+
+                let newArr = arr.slice(0, index - 1).concat(arr.slice(index + 1));
+
+                if (newArr.length == 2) {
+                    return true;
+                }
+
+                return checker(newArr, bracketsConfig);
+
+            }
+        }
+    }
 }
